b7c31f21d1f1ad115f1edee86896ae9d159775f4
Negarnbkhti42 <norob35@gmail.com>
2022-07-26 17:20:18 +0430

    Add elastic option to front

--- elastic_search.py
+++ elastic_search.py
@@ -5,25 +5,26 @@ from tqdm import tqdm
 class ElasticSearchResult:
     def __init__(self, cloud_id, elastic_user, elastic_password):
         self._INDEX = "docs"
-        self.client = Elasticsearch(cloud_id=cloud_id, basic_auth=(elastic_user, elastic_password))
+        self.client = Elasticsearch(
+            cloud_id=cloud_id, basic_auth=(elastic_user, elastic_password))
 
     def indexing(self, document_list):
         i = 0
         for doc in document_list:
             ans = self.client.index(index=self._INDEX, id=i, document=doc)
-            i+=1
-    
+            i += 1
 
     def get_index(self):
         print(self.client.indices.get(index=self._INDEX))
         print('done')
+
     def delete_doc(self):
         self.client.indices.delete(index=self._INDEX, ignore=[400, 404])
-    
-    def search(self, query, k: int=10, _from: int=0):
+
+    def search(self, query, k: int = 10, _from: int = 0):
         body = {
-            'from' : 0,
-            'size' : k,
+            'from': 0,
+            'size': k,
             'query': {
                 'match': {
                     'paragraphs': query
